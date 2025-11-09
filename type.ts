0db46ccb4b2decbc70c433b69c43cdff164bcbfd
Tushar Revankar <tusharrevankar999@gmail.com>
2025-11-09 21:22:21 +0530

    build fix

--- type.ts
+++ type.ts
@@ -2,7 +2,7 @@ export interface Products {
   _id: number;
   title: string;
   isNew: boolean;
-  oldPrice: number;
+  oldPrice: number | string;
   price: number;
   description: string;
   category: string;
