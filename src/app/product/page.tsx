f36601f18a663c9122fc7f5445d45f56a76574e6
Tushar Revankar <tusharrevankar999@gmail.com>
2025-11-09 21:51:13 +0530

    prod-fix

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -21,7 +21,7 @@ const ProuctPage = async ({ searchParams }: Props) => {
         <div>
           <p className="text-xl py-1 font-semibold">Tranding Products</p>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
-            {data?.map((item: Products) => (
+            {data?.map((item) => (
               <ProductsData key={item._id} item={item} />
             ))}
           </div>
